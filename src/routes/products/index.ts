689b75f4b6ab3dc74f94bae74f973d9a155438f6
RUYANGA <ruyangam15@gmail.com>
2025-04-16 09:51:25 +0200

    New

--- src/routes/products/index.ts
+++ src/routes/products/index.ts
@@ -1,12 +1,12 @@
 import { Router ,Request,Response,NextFunction} from "express";
-import { listProducts ,getProductById,creatProduct} from "../../controllers/products/products";
+import { listProducts ,getProductById,creatProduct, updateProduct, deleteProduct} from "../../controllers/products/products";
 
 const router=Router()
 
-
 router.get('/',listProducts)
-router.get('/:id',getProductById)
-
-
+router.get('/product/:id',getProductById)
+router.post('/product/create',creatProduct)
+router.put('/product/:id',updateProduct)
+router.delete('/product/:id',deleteProduct)
 
 export default router
