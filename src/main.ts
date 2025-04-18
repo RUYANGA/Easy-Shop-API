191a2e3b2cb510cb2e8aa4e001bea1c6b4bacb8a
RUYANGA <ruyangam15@gmail.com>
2025-04-18 18:41:23 +0200

    New

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import express ,{Request,Response,NextFunction,ErrorRequestHandler} from 'express'
 
 import productRoute from './routes/products/product'
