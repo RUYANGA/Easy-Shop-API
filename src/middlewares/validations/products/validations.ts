3e4581627f46f66afb8216e6f15e48383a76b536
RUYANGA <ruyangam15@gmail.com>
2025-04-19 14:31:03 +0200

    New

---
+++ src/middlewares/validations/products/validations.ts
@@ -0,0 +1,25 @@
+import { body } from "express-validator";
+
+
+export const product_validation=[
+    body('name')
+    .notEmpty()
+    .escape()
+    .withMessage('Name of product is required')
+    .isLength({min:3})
+    .withMessage('Name must be contain 3 character'),
+    body('price')
+    .notEmpty()
+    .withMessage('Please enter price')
+    .escape()
+    .isNumeric()
+    .withMessage('Price must be numbers'),
+    body('description')
+    .notEmpty()
+    .withMessage('Please enter description')
+    .escape()
+    .isString()
+    .withMessage('Description must be character')
+]
+
+
--- src/routes/products/product.ts
+++ src/routes/products/product.ts
@@ -7,11 +7,14 @@ import {
     deleteProduct
     } from "../../controllers/products/products";
 
+import {product_validation} from '../../middlewares/validations/products/validations'
+import {validateRequest} from '../../middlewares/auth/requestValidator'
+
 const router=Router()
 
 router.get('/',listProducts)
 router.get('/product/:id',getProductById)
-router.post('/product/create/:id',creatProduct)
+router.post('/product/create/:id',product_validation,validateRequest,creatProduct)
 router.put('/product/:id',updateProduct)
 router.delete('/product/:id',deleteProduct)
 
