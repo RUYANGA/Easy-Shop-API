15dcff454a3cdc0d1ba0c05f5e793ce0be2dfb4f
RUYANGA <ruyangam15@gmail.com>
2025-04-22 19:47:35 +0200

    new

--- src/routes/users/user.ts
+++ src/routes/users/user.ts
@@ -1,6 +1,6 @@
 import { Router } from "express"
 
-import {creatUser, Dashboard,verifyOtp,Login,resendOtp,userUpdate} from '../../controllers/users/user'
+import {creatUser, Dashboard,verifyOtp,Login,resendOtp,userUpdate,deleteAcount} from '../../controllers/users/user'
 
 import {signUp_Validation,verify_Otp,loginValidation,resendOtp_validation,} from '../../middlewares/validations/users/validator'
 
@@ -16,6 +16,7 @@ router.post('/user/resendotp',resendOtp_validation,validateRequest,resendOtp)
 router.post('/user/login',loginValidation,validateRequest,Login)
 router.get('/user/dashboard',AuthorizeRoles(['ADMIN']),Dashboard)
 router.put('/user/updates',AuthorizeRoles(['USER','ADMIN']),userUpdate)
+router.delete('/user/delete',AuthorizeRoles(['USER','ADMIN']),deleteAcount)
 
 
 export default router
